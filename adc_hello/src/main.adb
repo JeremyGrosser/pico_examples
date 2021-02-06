a1fa2caf727454f748c32bf6f791f58d3eea5197
Jeremy Grosser <jeremy@synack.me>
2021-02-06 01:01:58 -0800

    clean up adc demo

--- adc_hello/src/main.adb
+++ adc_hello/src/main.adb
@@ -5,32 +5,27 @@
 --
 with Ada.Text_IO; use Ada.Text_IO;
 with HAL; use HAL;
+with RP.ADC; use RP.ADC;
 with RP.SysTick;
 with RP.Device;
 with RP.Clock;
-with RP.ADC;
 with Pico;
 
 procedure Main is
-   type Microvolts is new Integer;
-   VREF   : constant Microvolts := 3_300_000;
-   Counts : RP.ADC.Analog_Value;
-   Result : Microvolts;
+   VSYS : Microvolts;
 begin
    RP.Clock.Initialize (Pico.XOSC_Frequency);
    RP.Clock.Enable (RP.Clock.ADC);
-   RP.ADC.Configure (Channel => 0);
+   RP.ADC.Configure (0);
+   RP.ADC.Configure (Pico.VSYS_DIV_3);
+   RP.ADC.Configure (Temperature_Sensor);
 
    RP.SysTick.Enable;
    loop
-      Counts := RP.ADC.Read (Channel => 0);
-      if Counts = 0 then
-         Result := 0;
-      else
-         Result := (VREF / Microvolts (RP.ADC.Analog_Value'Last)) * Microvolts (Counts);
-      end if;
-      Put_Line ("Raw value:" & Counts'Image & "," & Result'Image & "μv");
-
+      VSYS := Read_Microvolts (Pico.VSYS_DIV_3) * 3;
+      Put_Line ("Channel 0:  " & Read_Microvolts (0)'Image & "μv");
+      Put_Line ("VSYS:       " & VSYS'Image & "μv");
+      Put_Line ("Temperature:" & Temperature'Image & "°C");
       RP.Device.SysTick.Delay_Milliseconds (1000);
    end loop;
 end Main;
