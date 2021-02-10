81bb2e932020e73eb034149e8b5898ad45046dc0
Jeremy Grosser <jeremy@synack.me>
2021-02-10 15:37:44 -0800

    Change PWM example to use LED for output, no scope required!

--- pwm/src/main.adb
+++ pwm/src/main.adb
@@ -13,26 +13,24 @@ with Pico;
 
 procedure Main is
    use type RP.PWM.Period;
-   PWM15 : constant PWM_Point := To_PWM (Pico.GP15);
+   PWM_LED : constant PWM_Point := To_PWM (Pico.LED);
    Duty_Cycle : Period := 0;
 begin
    RP.Clock.Initialize (Pico.XOSC_Frequency);
    RP.Clock.Enable (PERI);
    RP.GPIO.Enable;
 
-   Pico.LED.Configure (Output, Pull_Up);
-   Pico.GP15.Configure (Analog, Floating, RP.GPIO.PWM);
-   Set_Phase_Correction (PWM15.Slice, True);
-   Set_Clock_Divider (PWM15.Slice, Clock_Divider (Frequency (SYS) / 10_000_000));
-   Set_Interval (PWM15.Slice, 10);
-   Set_Duty_Cycle (PWM15, Duty_Cycle);
-   Enable (PWM15.Slice);
+   Pico.LED.Configure (Analog, Floating, RP.GPIO.PWM);
+   Set_Phase_Correction (PWM_LED.Slice, True);
+   Set_Clock_Divider (PWM_LED.Slice, Clock_Divider (Frequency (SYS) / 10_000_000));
+   Set_Interval (PWM_LED.Slice, 100);
+   Set_Duty_Cycle (PWM_LED, Duty_Cycle);
+   Enable (PWM_LED.Slice);
 
    SysTick.Enable;
    loop
-      SysTick.Delay_Milliseconds (100);
-      Duty_Cycle := (Duty_Cycle + 1) mod 10;
-      Set_Duty_Cycle (PWM15, Duty_Cycle);
-      Pico.LED.Toggle;
+      SysTick.Delay_Milliseconds (10);
+      Duty_Cycle := (Duty_Cycle + 1) mod 100;
+      Set_Duty_Cycle (PWM_LED, Duty_Cycle);
    end loop;
 end Main;
