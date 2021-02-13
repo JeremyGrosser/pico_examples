076f690086719ecf034efcb6b0c92f145d729bf9
Jeremy Grosser <jeremy@synack.me>
2021-02-12 16:31:49 -0800

    pio example, not working yet

--- pio_blink/src/main.adb
+++ pio_blink/src/main.adb
@@ -3,28 +3,33 @@
 --
 --  SPDX-License-Identifier: BSD-3-Clause
 --
-with HAL.GPIO;   use HAL.GPIO;
-with RP.Device;  use RP.Device;
-with RP.GPIO;    use RP.GPIO;
+with HAL.GPIO;  use HAL.GPIO;
+with RP.Device; use RP.Device;
+with RP.PIO;    use RP.PIO;
+with RP.GPIO;   use RP.GPIO;
 with RP.Clock;
 with Pico;
-with HAL; use HAL;
-
-with hello;
-
---  XXX: THIS EXAMPLE DOES NOT WORK YET
+with Hello;
 
 procedure Main is
+   SM : PIO_Point := (PIO_0'Access, SM_0);
 begin
    RP.Clock.Initialize (Pico.XOSC_Frequency);
+   RP.GPIO.Enable;
 
-   --PIO_0.Load (hello.Program_Instructions);
+   Pico.LED.Configure (Output, Pull_Up, PIO0);
+
+   PIO_0.Enable;
+   SM.Set_Out_Pins (Pico.LED, 1);
+   SM.Set_Divider (1.0);
+   SM.Load (Hello.Hello_Program_Instructions, Hello.Hello_Wrap_Target, Hello.Hello_Wrap);
+   SM.Enable;
 
    SysTick.Enable;
    loop
-      --  PIO_0.Put (1);
+      SM.Transmit (1);
       SysTick.Delay_Milliseconds (100);
-      --  PIO_0.Put (0);
+      SM.Transmit (0);
       SysTick.Delay_Milliseconds (100);
    end loop;
 end Main;
