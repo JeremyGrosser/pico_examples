158ef3220cae2a9710517958c192feb7631970ea
Jeremy Grosser <jeremy@synack.me>
2021-02-15 13:34:12 -0800

    Fix PIO example

--- pio_blink/src/main.adb
+++ pio_blink/src/main.adb
@@ -3,7 +3,6 @@
 --
 --  SPDX-License-Identifier: BSD-3-Clause
 --
-with HAL.GPIO;  use HAL.GPIO;
 with RP.Device; use RP.Device;
 with RP.PIO;    use RP.PIO;
 with RP.GPIO;   use RP.GPIO;
@@ -17,12 +16,16 @@ begin
    RP.Clock.Initialize (Pico.XOSC_Frequency);
    RP.GPIO.Enable;
 
-   Pico.LED.Configure (Output, Pull_Up, PIO0);
+   Pico.LED.Configure (Analog, Floating, PIO0);
 
    PIO_0.Enable;
-   SM.Set_Out_Pins (Pico.LED, 1);
-   SM.Set_Divider (1.0);
+
+   SM.Disable;           --  stop executing
+   SM.Restart;           --  flush fifos
+   SM.Set_Divider (2.5); --  50 MHz clock
+   SM.Set_Out_Pins (Base => Pico.LED, Count => 1);
    SM.Load (Hello.Hello_Program_Instructions, Hello.Hello_Wrap_Target, Hello.Hello_Wrap);
+   SM.Execute (16#0000#); -- jmp 0
    SM.Enable;
 
    SysTick.Enable;
