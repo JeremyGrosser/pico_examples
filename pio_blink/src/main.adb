3b6683d4b498cd7d2eb8f423e64238aa74d7301c
Jeremy Grosser <jeremy@synack.me>
2021-02-22 03:16:55 -0800

    Refactor PIO example

--- pio_blink/src/main.adb
+++ pio_blink/src/main.adb
@@ -4,38 +4,39 @@
 --  SPDX-License-Identifier: BSD-3-Clause
 --
 with RP.Device; use RP.Device;
-with RP.PIO;    use RP.PIO;
 with RP.GPIO;   use RP.GPIO;
-with HAL;       use HAL;
 with RP.Clock;
+with RP.PIO;
 with Pico;
 with Hello;
 
 procedure Main is
-   SM_0 : PIO_Point :=
-      (Periph => PIO_0'Access,
-       SM     => 0);
 begin
    RP.Clock.Initialize (Pico.XOSC_Frequency);
    RP.GPIO.Enable;
+
    Pico.LED.Configure (Output, Pull_Up, PIO0);
 
-   SysTick.Enable;
-
    RP.PIO.Initialize;
-   SM_0.Disable;
-   SM_0.Set_Divider (2.5); --  50 MHz clock
-   SM_0.Set_Out_Pins (Base => Pico.LED, Count => 1);
-   SM_0.Set_Set_Pins (Base => Pico.LED, Count => 1);
-   SM_0.Load (Hello.Hello_Program_Instructions, Hello.Hello_Wrap_Target, Hello.Hello_Wrap);
-   SM_0.Restart;
-   SM_0.Execute (16#e081#); --  set pindirs, 1
-   SM_0.Enable;
+   PIO_0.Configure (0,
+      (Clock_Divider  => RP.PIO.To_Divider (Frequency => 50_000_000),
+       Out_Base       => Pico.LED,
+       Out_Count      => 1,
+       Set_Base       => Pico.LED,
+       Set_Count      => 1,
+       others         => <>));
+   PIO_0.Load (0,
+      Prog        => Hello.Hello_Program_Instructions,
+      Wrap_Target => Hello.Hello_Wrap_Target,
+      Wrap        => Hello.Hello_Wrap);
+   PIO_0.Execute (0, 16#e081#); --  set pindirs, 1
+   PIO_0.Enable ((0 => True, others => False));
 
+   SysTick.Enable;
    loop
-      SM_0.Transmit (1);
+      PIO_0.Transmit (0, 1);
       SysTick.Delay_Milliseconds (100);
-      SM_0.Transmit (0);
+      PIO_0.Transmit (0, 0);
       SysTick.Delay_Milliseconds (100);
    end loop;
 end Main;
