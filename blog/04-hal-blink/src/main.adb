8e249c48d37a13dfb754daa78c6b0d9d6707106b
Jeremy Grosser <jeremy@synack.me>
2021-03-02 23:23:24 -0800

    remove unnecessary RP.Clock.Initialize

--- blog/04-hal-blink/src/main.adb
+++ blog/04-hal-blink/src/main.adb
@@ -1,14 +1,11 @@
 with Ada.Real_Time; use Ada.Real_Time;
 with RP.GPIO; use RP.GPIO;
-with RP.Clock;
 
 procedure Main is
     LED        : GPIO_Point := (Pin => 25);
     Next_Blink : Time := Clock;
 begin
-    RP.Clock.Initialize (XOSC_Frequency => 12_000_000);
     LED.Configure (Output);
-
     loop
        LED.Toggle;
        delay until Next_Blink;
