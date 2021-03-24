e31225062f107175f1b7ca478223b2ac1b1d15ab
Jeremy Grosser <jeremy@synack.me>
2021-03-24 05:11:58 -0700

    Rewrite pio_blink to match new RP.PIO API

--- pio_blink/src/main.adb
+++ pio_blink/src/main.adb
@@ -11,32 +11,31 @@ with Pico;
 with Hello;
 
 procedure Main is
+   Config : RP.PIO.PIO_SM_Config := RP.PIO.Default_SM_Config;
+   SM     : constant RP.PIO.PIO_SM := 0;
 begin
    RP.Clock.Initialize (Pico.XOSC_Frequency);
    RP.GPIO.Enable;
 
    Pico.LED.Configure (Output, Pull_Up, PIO0);
 
-   RP.PIO.Initialize;
-   PIO_0.Configure (0,
-      (Clock_Divider  => RP.PIO.To_Divider (Frequency => 50_000_000),
-       Out_Base       => Pico.LED,
-       Out_Count      => 1,
-       Set_Base       => Pico.LED,
-       Set_Count      => 1,
-       others         => <>));
-   PIO_0.Load (0,
-      Prog        => Hello.Hello_Program_Instructions,
+   RP.PIO.Set_Out_Pins (Config, Pico.LED.Pin, 1);
+   RP.PIO.Set_Set_Pins (Config, Pico.LED.Pin, 1);
+   RP.PIO.Set_Wrap (Config,
       Wrap_Target => Hello.Hello_Wrap_Target,
       Wrap        => Hello.Hello_Wrap);
-   PIO_0.Execute (0, 16#e081#); --  set pindirs, 1
-   PIO_0.Enable ((0 => True, others => False));
+   RP.PIO.Set_Clock_Frequency (Config, 50_000_000);
+
+   RP.PIO.Enable (PIO_0);
+   RP.PIO.Load (PIO_0, Hello.Hello_Program_Instructions, 0);
+   RP.PIO.SM_Initialize (PIO_0, SM, 0, Config);
+   RP.PIO.Set_Pin_Direction (PIO_0, SM, Pico.LED.Pin, RP.PIO.Output);
 
    SysTick.Enable;
    loop
-      PIO_0.Transmit (0, 1);
+      RP.PIO.Put (PIO_0, SM, 1);
       SysTick.Delay_Milliseconds (100);
-      PIO_0.Transmit (0, 0);
+      RP.PIO.Put (PIO_0, SM, 0);
       SysTick.Delay_Milliseconds (100);
    end loop;
 end Main;
