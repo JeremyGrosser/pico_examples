80395d36d7d949aca3f992c32b2cef3c8653d32c
Jeremy Grosser <jeremy@synack.me>
2021-03-25 08:40:47 -0700

    Fix audio pack example

--- pimoroni_audio_pack/src/main.adb
+++ pimoroni_audio_pack/src/main.adb
@@ -3,10 +3,8 @@
 --
 --  SPDX-License-Identifier: BSD-3-Clause
 --
-with Interfaces; use Interfaces;
-with RP.Device;  use RP.Device;
-with RP.GPIO;    use RP.GPIO;
-with RP.Clock;
+with RP.Device;    use RP.Device;
+with RP.GPIO;      use RP.GPIO;
 with Pico.Pimoroni.Audio_Pack;
 with Pico.Audio_I2S;
 with Pico;
@@ -15,14 +13,17 @@ with Simple_Synthesizer;
 with HAL.Audio;
 
 procedure Main is
-   Line_Out : Pico.Audio_I2S.I2S_Device renames Pico.Pimoroni.Audio_Pack.I2S;
-   Mute     : RP.GPIO.GPIO_Point renames Pico.Pimoroni.Audio_Pack.MUTE;
-   Synth    : Simple_Synthesizer.Synthesizer
+   Line_Out  : Pico.Audio_I2S.I2S_Device renames Pico.Pimoroni.Audio_Pack.I2S;
+   Mute      : RP.GPIO.GPIO_Point renames Pico.Pimoroni.Audio_Pack.MUTE;
+
+   Synth : Simple_Synthesizer.Synthesizer
       (Stereo    => True,
-       Amplitude => Integer (Integer_16'Last / 8));
-   Buffer   : HAL.Audio.Audio_Buffer (1 .. 1024);
+       Amplitude => 1000);
+
+   Sample_Rate : constant HAL.Audio.Audio_Frequency := HAL.Audio.Audio_Freq_44kHz;
+   Buffer      : HAL.Audio.Audio_Buffer
+      (1 .. HAL.Audio.Audio_Frequency'Enum_Rep (Sample_Rate) / 10);
 begin
-   RP.Clock.Initialize (Pico.XOSC_Frequency);
    RP.GPIO.Enable;
 
    Pico.LED.Configure (Output);
@@ -32,8 +33,8 @@ begin
    Mute.Configure (Output, Pull_Both);
    Mute.Clear;
 
-   Line_Out.Initialize (HAL.Audio.Audio_Freq_44kHz);
-   Synth.Set_Frequency (HAL.Audio.Audio_Freq_44kHz);
+   Line_Out.Initialize (Sample_Rate);
+   Synth.Set_Frequency (Sample_Rate);
    Synth.Set_Note_Frequency (440.0);
    Synth.Receive (Buffer);
 
@@ -41,6 +42,5 @@ begin
 
    loop
       Line_Out.Transmit (Buffer);
-      Pico.LED.Toggle;
    end loop;
 end Main;
