89e68f7ad4694a12f4132c4638cd9f0c887faf95
Jeremy Grosser <jeremy@synack.me>
2021-05-28 19:46:08 -0700

    SPI slave transmit example

--- spi_loopback/src/main.adb
+++ spi_loopback/src/main.adb
@@ -41,6 +41,7 @@ procedure Main is
       SPI_Master.Configure (Master_Config);
       SPI_Slave.Configure (Slave_Config);
 
+      --  Master to slave
       for I in 1 .. 10 loop
          Data_Out (1) := UInt8 (I);
          SPI_Master.Transmit (Data_Out, Status_Out);
@@ -67,20 +68,62 @@ procedure Main is
       SPI_Master.Configure (Master_Config);
       SPI_Slave.Configure (Slave_Config);
 
-      --  Master transmits
       for I in 1 .. 10 loop
          for J in Data_Out'Range loop
             Data_Out (J) := UInt16 (I) + 1;
          end loop;
          SPI_Master.Transmit (Data_Out, Status_Out);
-         pragma Assert (Status_Out = Ok, "Transmit error");
+         pragma Assert (Status_Out = Ok, "Transmit error: " & Status_Out'Image);
          SPI_Slave.Receive (Data_In, Status_In);
-         pragma Assert (Status_In = Ok, "Receive error");
+         pragma Assert (Status_In = Ok, "Receive error: " & Status_In'Image);
          pragma Assert (Data_Out = Data_In, "Data mismatch");
       end loop;
 
       Ada.Text_IO.Put_Line ("PASS");
    end Test_16;
+
+   procedure Test_Slave
+      (Name          : String;
+       Master_Config : SPI_Configuration;
+       Slave_Config  : SPI_Configuration)
+   is
+      --  Things need to happen in the following order:
+      --    1. Set A := I, B := not I
+      --    2. Put A in the slave's transmit FIFO
+      --    3. Transmit B from the master to generate a clock
+      --    4. Read A from the master FIFO into B, check A = B
+      --    5. Read B from the slave FIFO into A, check A = not B
+      A : SPI_Data_16b (1 .. 1) := (others => 0);
+      B : SPI_Data_16b (1 .. 1) := (others => 0);
+      Status : SPI_Status;
+   begin
+      Ada.Text_IO.Put (Name & "...");
+      SPI_Master.Configure (Master_Config);
+      SPI_Slave.Configure (Slave_Config);
+
+      --  Slave to master
+      for I in 16#5A5A# .. 16#5A6A# loop
+         A (1) := UInt16 (I);
+         B (1) := not A (1);
+
+         SPI_Slave.Transmit (A, Status);
+         pragma Assert (Status = Ok, "Slave transmit: " & Status'Image);
+
+         SPI_Master.Transmit (B, Status);
+         pragma Assert (Status = Ok, "Master transmit: " & Status'Image);
+
+         SPI_Master.Receive (B, Status);
+         pragma Assert (Status = Ok, "Master receive: " & Status'Image);
+         pragma Assert (A = B, "Master received incorrect value: " &
+            "A = " & A (1)'Image & ", " &
+            "B = " & B (1)'Image);
+
+         SPI_Slave.Receive (A, Status);
+         pragma Assert (Status = Ok, "Slave receive: " & Status'Image);
+         pragma Assert (A (1) = not B (1));
+      end loop;
+      Ada.Text_IO.Put_Line ("PASS");
+   end Test_Slave;
 
 begin
    RP.Clock.Initialize (Pico.XOSC_Frequency);
@@ -156,6 +199,18 @@ begin
          Data_Size => Data_Size_16b,
          others    => <>),
       Transfer_Length => 8);
+
+   Test_Slave ("Slave transmit",
+      Master_Config =>
+        (Role      => Master,
+         Baud      => 10_000_000,
+         Data_Size => Data_Size_16b,
+         others    => <>),
+      Slave_Config =>
+        (Role      => Slave,
+         Baud      => 10_000_000,
+         Data_Size => Data_Size_16b,
+         others    => <>));
 
    Ada.Text_IO.Put_Line ("ALL TESTS PASS");
    loop
