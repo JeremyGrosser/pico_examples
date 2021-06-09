dd4ba25bb23258ac0397df46fee6ab91a89eedb2
Jeremy Grosser <jeremy@synack.me>
2021-06-09 10:33:58 -0700

    Updated to match new PWM interface

--- pwm/src/main.adb
+++ pwm/src/main.adb
@@ -3,34 +3,41 @@
 --
 --  SPDX-License-Identifier: BSD-3-Clause
 --
-with HAL.GPIO; use HAL.GPIO;
-with RP.Device; use RP.Device;
-with RP.Clock; use RP.Clock;
-with RP.GPIO; use RP.GPIO;
+with HAL;    use HAL;
 with RP.PWM; use RP.PWM;
-with RP; use RP;
+with RP.Device;
+with RP.Clock;
+with RP.GPIO;
 with Pico;
 
 procedure Main is
-   use type RP.PWM.Period;
-   PWM_LED : PWM_Point := To_PWM (Pico.LED);
+   PWM_LED    : constant PWM_Slice := To_PWM (Pico.LED).Slice;
+   Reload     : constant Period := 1_000;
    Duty_Cycle : Period := 0;
 begin
    RP.Clock.Initialize (Pico.XOSC_Frequency);
-   RP.Clock.Enable (PERI);
-   RP.GPIO.Enable;
+   RP.Device.Timer.Enable;
 
-   Pico.LED.Configure (Analog, Floating, RP.GPIO.PWM);
-   Set_Phase_Correction (PWM_LED.Slice, True);
-   Set_Frequency (PWM_LED.Slice, 10_000_000);
-   Set_Interval (PWM_LED.Slice, 100);
-   Set_Duty_Cycle (PWM_LED, Duty_Cycle);
-   Enable (PWM_LED.Slice);
+   Pico.LED.Configure
+      (RP.GPIO.Output, RP.GPIO.Floating, RP.GPIO.PWM);
 
-   SysTick.Enable;
+   Set_Frequency (PWM_LED,
+      Frequency => 1_000_000);
+
+   --  When enabled, the PWM counts up to Clocks then toggles the output
+   Set_Interval (PWM_LED,
+      Clocks => Reload);
+
+   Enable (PWM_LED);
+
    loop
-      SysTick.Delay_Milliseconds (10);
-      Duty_Cycle := (Duty_Cycle + 1) mod 100;
-      Set_Duty_Cycle (PWM_LED, Duty_Cycle);
+      --  Each PWM slice has two output channels. Only one is connected to the
+      --  GPIO here, so setting the duty cycle of the other has no effect.
+      Set_Duty_Cycle (PWM_LED,
+         Clocks_A => Duty_Cycle,
+         Clocks_B => Duty_Cycle);
+
+      RP.Device.Timer.Delay_Milliseconds (10);
+      Duty_Cycle := (Duty_Cycle + 10) mod Reload;
    end loop;
 end Main;
