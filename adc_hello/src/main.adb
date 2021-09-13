f2f9060b300a9c6257d3adbd3fd3641f00a46b7f
Jeremy Grosser <jeremy@synack.me>
2021-09-12 20:16:40 -0700

    Update ADC example to set SMPS_PS high during readings

--- adc_hello/src/main.adb
+++ adc_hello/src/main.adb
@@ -5,6 +5,7 @@
 --
 with Ada.Text_IO; use Ada.Text_IO;
 with RP.Device;   use RP.Device;
+with RP.GPIO;     use RP.GPIO;
 with RP.ADC;      use RP.ADC;
 with RP.Clock;
 with Pico;
@@ -13,17 +14,27 @@ procedure Main is
    VSYS : Microvolts;
 begin
    RP.Clock.Initialize (Pico.XOSC_Frequency);
+
+   --  The Pico's power regulator dynamically adjusts it's switching frequency
+   --  based on load. This introduces noise that can affect ADC readings. The
+   --  Pico datasheet recommends setting Power Save pin high while performing
+   --  ADC measurements to minimize this noise, at tne expense of higher power
+   --  consumption.
+   Pico.SMPS_PS.Configure (Output, Pull_Up);
+   Pico.SMPS_PS.Clear;
+
    RP.ADC.Enable;
    RP.ADC.Configure (0);
    RP.ADC.Configure (Pico.VSYS_DIV_3);
-   RP.ADC.Configure (Temperature_Sensor);
 
    SysTick.Enable;
    loop
-      VSYS := Read_Microvolts (Pico.VSYS_DIV_3) * 3;
-      Put_Line ("Channel 0:  " & Read_Microvolts (0)'Image & "μv");
+      Pico.SMPS_PS.Set;
+      VSYS := RP.ADC.Read_Microvolts (Pico.VSYS_DIV_3) * 3;
+      Put_Line ("Channel 0:  " & RP.ADC.Read_Microvolts (0)'Image & "μv");
       Put_Line ("VSYS:       " & VSYS'Image & "μv");
-      Put_Line ("Temperature:" & Temperature'Image & "°C");
+      Put_Line ("Temperature:" & RP.ADC.Temperature'Image & "°C");
+      Pico.SMPS_PS.Clear;
       SysTick.Delay_Milliseconds (1000);
    end loop;
 end Main;
