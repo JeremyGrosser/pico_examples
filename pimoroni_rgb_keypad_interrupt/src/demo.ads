3a5b2bb9074a794868c537534701d3cc679fa079
Jeremy Grosser <jeremy@synack.me>
2023-03-03 13:50:15 -0800

    Remove RGB Keypad interrupts demo

--- pimoroni_rgb_keypad_interrupt/src/demo.ads
+++
@@ -1,9 +0,0 @@
---
---  Copyright 2021 (C) Jeremy Grosser <jeremy@synack.me>
---
---  SPDX-License-Identifier: BSD-3-Clause
---
-package Demo is
-   procedure Initialize;
-   procedure Pad_Changed;
-end Demo;
